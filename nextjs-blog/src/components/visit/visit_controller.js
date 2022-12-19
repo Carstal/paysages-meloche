c93071f6d741bef1b6bcc5d5c7727b9b17b83448
Carstal <43147122+Carstal@users.noreply.github.com>
2022-12-19 13:53:49 -0500

    Defined goals and functions of visit controller

--- nextjs-blog/src/components/visit/visit_controller.js
+++ nextjs-blog/src/components/visit/visit_controller.js
@@ -1,2 +1,19 @@
 //Visit Controller
 //Handles all requests and methods for the visit service & class
+
+//TODO: Complete Visit controller
+
+
+//TODO: Create Visit
+
+
+//TODO: Update Visit
+
+
+//TODO: Get Visit By Id
+
+
+//TODO: Get All Visits
+
+
+//TODO: Get All Visits
