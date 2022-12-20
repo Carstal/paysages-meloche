a2833eec20e2cca511a01d23f247d46890b6a46f
Carstal <43147122+Carstal@users.noreply.github.com>
2022-12-19 20:14:17 -0500

    controller update delete create function

--- nextjs-blog/src/components/visit/visit_controller.js
+++ nextjs-blog/src/components/visit/visit_controller.js
@@ -6,9 +6,11 @@ import{
     getAllVisits,
     getVisitByVisitId,
     getVisitsByProjectId,
+    addVisit,
     updateVisit,
     deleteVisitById
 } from "./visit_controller";
+import { addVisit } from "./visit_service";
 //Import Visit class module
 const Visit = require("./Visit");
 
@@ -24,8 +26,42 @@ const visitOne = new Visit(
 //TODO: Complete Visit controller
 
 //TODO: Create Visit
+export const createVisit = (req,res) => {
+    const visitId = req.params.visitId;
+    const projectId = req.params.projectId;
+    const employeeIds = req.params.employeeIds;
+    const startDate = req.params.startDate;
+    const endDate = req.params.endDate;
+
+    const newVisit = new Visit(visitId, projectId, employeeIds, startDate, endDate);
+
+    const dbInsert = addVisit(newVisit);
+
+    return res.json(dbInsert);
+};
 
 //TODO: Update Visit
+export const updateVisitInfo = (req,res) => {
+    const visitId = req.params.visitId;
+    const projectId = req.params.projectId;
+    const employeeIds = req.params.employeeIds;
+    const startDate = req.params.startDate;
+    const endDate = req.params.endDate;
+
+    const newVisit = new Visit(visitId, projectId, employeeIds, startDate, endDate);
+
+    const dbInsert = updateVisit(newVisit);
+
+    return res.json(dbInsert);
+};
+
+//TODO: Delete Visit By Id
+export const deleteVisit = (req,res) => {
+    const visitId = req.params.visitId;
+    const deletedVisit = deleteVisit(visitId);
+
+    return res.json(deleteVisit);
+};
 
 
 //TODO: Get Visit By Id
@@ -39,14 +75,14 @@ export const getVisit = (req,res) => {
 //TODO: Get All Visits
 export const getProjectVisits = (req,res) => {
     const projectId = req.params.projectId;
-    const visits = getVisitsByProjectId(projectId);
+    const projectVisits = getVisitsByProjectId(projectId);
 
-    return res.json(visits);
+    return res.json(projectVisits);
 };
 
 //TODO: Get All Visits
 export const getVisits = (req,res) => {
-    const visits = getAllVisits();
+    const allVisits = getAllVisits();
 
-    return res.json(visits);
+    return res.json(allVisits);
 };
