40612be5b7f18021fc43feb0e42228f0e2016538
Mohaned <mohanedbouzaidi@gmail.com>
2023-01-03 13:45:12 -0500

    Start of internationalization

---
+++ nextjs-blog/pages/Test1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "../src/Translation/i18n";
+ 
+export const ExampleComponent = () => {
+const { t } = useTranslation();
+ return (
+   <div>
+      <p>
+        {t("welcome")}
+      </p>
+   </div>
+ );
+};
---
+++ nextjs-blog/pages/Test2.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Test1 } from "./Test1";
+ 
+export default function App() {
+ return (
+   <div className="App">
+     <Test1 />
+   </div>
+ );
+}
