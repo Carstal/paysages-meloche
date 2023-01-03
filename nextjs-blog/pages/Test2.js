5f0195421c185ca969123237570bf0eff6cabc72
Mohaned <mohanedbouzaidi@gmail.com>
2023-01-03 14:30:46 -0500

    Added buttons for localisation

--- nextjs-blog/pages/Test2.js
+++ nextjs-blog/pages/Test2.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { Test1 } from "./Test1";
+import {i18n} from "../src/Translation/i18n";
+
  
 export default function App() {
+    const [language, setLanguage] = useState('en');
+
+    const handleOnclick=(e)=>{
+        e.preventDefault();
+        setLanguage(e.target.value);
+        i18n.changeLanguage(e.target.value);
+      }
  return (
    <div className="App">
-     <Test1 />
+    <button value='fr' onClick={handleOnclick}>
+        French
+     </button>
+     <button value='en' onClick={handleOnclick}>
+        English
+     </button>
+     <Test1 lang={language}/>
+
    </div>
  );
 }
