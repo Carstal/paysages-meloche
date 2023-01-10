96b6869bedb5e9466c3e0dd5e15912f9135bdbb4
Carstal <43147122+Carstal@users.noreply.github.com>
2023-01-10 15:02:45 -0500

    get all visits api

---
+++ nextjs-blog/pages/api/visit/all.js
@@ -0,0 +1,30 @@
+import { getVisits } from "../../../src/components/visit/visit_controller"
+
+export default async function handler(req,res){
+
+    const allVisits = await getVisits();
+    console.log(allVisits);
+
+    res.status(200).json(allVisits);
+    // res.status(200).json([{
+    //     visit_id: 1,
+    //     project_id: 1,
+    //     employee_ids: "12,34,54",
+    //     start_date: "2022-12-23",
+    //     end_date: "2022-12-23"
+    // },
+    // {
+    //     visit_id: 2,
+    //     project_id: 2,
+    //     employee_ids: "99,87",
+    //     start_date: "2022-12-23",
+    //     end_date: "2022-12-23"
+    // },
+    // {
+    //     visit_id: 3,
+    //     project_id: 3,
+    //     employee_ids: "12,54",
+    //     start_date: "2022-12-23",
+    //     end_date: "2022-12-23"
+    // }]);
+}
