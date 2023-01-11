1e7bc9e9bb12c6d29bb6e1106c269a4d70daf840
Mohaned <mohanedbouzaidi@gmail.com>
2023-01-10 21:20:45 -0500

    erased some code

--- nextjs-blog/pages/api/postProject.js
+++ nextjs-blog/pages/api/postProject.js
@@ -1,3 +1,4 @@
+import { IncrementalCache } from "next/dist/server/lib/incremental-cache";
 import clientPromise from "../../lib/mongodb";
 
 export default async (req, res) => {
@@ -6,9 +7,11 @@ export default async (req, res) => {
     const db = client.db("FinalProject");
     let data = req.body;
 
-    const create = await db.collection("Project").insertOne(data)
+    
 
-    res.json(data);
+   const create = await db.collection("Project").insertOne(data)
+
+    res.json(create);
   } catch (e) {
     console.error(e);
     throw new Error(e).message;
