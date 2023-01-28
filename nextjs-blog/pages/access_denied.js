afa1edbc5526ccc697bfaab377d2ee4934376cde
YanBurton <yanburton03@gmail.com>
2023-01-28 15:15:51 -0500

    Added access denied page

---
+++ nextjs-blog/pages/access_denied.js
@@ -0,0 +1,167 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+// import Profile from './profile';
+import Profile from './/profile/index';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import "../src/Translation/i18n";
+import i18n from "i18next";
+
+
+export default function Home() {
+  const [language, setLanguage] = useState('en');
+
+  const { t } = useTranslation();
+
+
+  const handleOnclick = (e) => {
+    e.preventDefault();
+    setLanguage(e.target.value);
+    if (i18n && i18n.changeLanguage) {
+      i18n.changeLanguage(e.target.value);
+    }
+  }
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{t("EmailV")}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <div className='logo'>
+          <h2>Paysages Meloche</h2>
+        </div>
+        <div className='services'>
+        </div>
+        {Profile()}
+        <button class={styles.loginbutton} value='fr' onClick={handleOnclick}>
+          French
+        </button>
+        <button class={styles.loginbutton} value='en' onClick={handleOnclick}>
+          English
+        </button>
+      </header>
+      <main>
+        <h1 className={styles.title}>
+          You do not have permissions to complete this operation.
+        </h1>
+      </main>
+
+      <footer>
+        <p>Created By Carlo Staltari, Mohaned Bouzaidi & Yan Burton
+          <br />
+          Champlain College ECP Final Project 2022-2023</p>
+      </footer>
+
+      <style jsx>{`
+        header {
+          width: 100vw;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          background: #222222;
+          color: #FFFFFF;
+        }
+        
+        .services {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          margin: auto;
+        }
+        .services div {
+          display: flex;
+          justify-content: center;
+          width: 18vw;
+          cursor: 'pointer';
+        }
+        .services div :hover {
+          background-colour: red;
+        }
+        .logo{
+          display: flex;
+          justify-content: center;
+          width: 15vw;
+        }
+        .login{
+          display: flex;
+          justify-content: center;
+          width: 15vw;
+          float: right;
+
+        }
+        .services{
+          display: flex;
+          justify-content: center;
+          width: 60vw;
+        }
+        .login button{
+          height: 7vh;
+          width: 10vw;
+          background: #00B45D;
+          border-radius: 40px;
+          color: #FFFFFF;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+          font-weight: bold;
+          float: right;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          background: #333333;
+          color: #FFFFFF;
+          width: 100vw;
+        }
+        footer {
+          width: 100vw;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background: #222222;
+          color: #FFFFFF;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer p {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
