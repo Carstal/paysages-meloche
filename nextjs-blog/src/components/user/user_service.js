8a90ca99df352f2f69ff6e7f932383dc36774cab
Carstal <43147122+Carstal@users.noreply.github.com>
2023-01-29 16:47:42 -0500

    is user employee

--- nextjs-blog/src/components/user/user_service.js
+++ nextjs-blog/src/components/user/user_service.js
@@ -11,8 +11,21 @@ export async function getUserById(id) {
         const result = await db.collection("User").findOne({
         user_id: id,
         });
-        return post
+        return result
     } catch (e) {
+        console.error(e);
+        throw new Error(e).message;
+    }
+};
+
+export async function isEmployee(id){
+    try{
+        const user = await getUserById(id)
+        const employee = user.isEmployee
+
+        return employee
+    }
+    catch(e){
         console.error(e);
         throw new Error(e).message;
     }
