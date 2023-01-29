d85cd33ddc9c3ba915b7f7d2af734c5aad4eba9c
YanBurton <yanburton03@gmail.com>
2023-01-29 18:18:57 -0500

    Protected /api/forms/form so that it can only update the employee status if the user sending the request is recognized as an admin.

--- nextjs-blog/pages/api/forms/form.js
+++ nextjs-blog/pages/api/forms/form.js
@@ -5,17 +5,21 @@ export default async function handler(req, res) {
     // Get data submitted in request's body.
     var body = req.body
     const session = await getSession(req, res);
+    const role = session.user.userRoles
 
     // converts is_employee to boolean
     if(body.is_employee != undefined){
-        var value = body.is_employee
-        var fix = (value === "true")
-        body = {email: body.email, first_name: body.first_name, last_name: body.last_name, phone_number: body.phone_number, is_employee: fix}
+        if(role == "Admin"){
+            var value = body.is_employee
+            var fix = (value === "true")
+            body = {email: body.email, first_name: body.first_name, last_name: body.last_name, phone_number: body.phone_number, is_employee: fix}
+        } else {
+            res.redirect("/access_denied")
+        }
     }
 
     try{
-        const email = session.user.email
-        const role = session.user.userRoles
+        const email = session.user.email     
         if(email == body.email || role == "Admin"){
             console.log("authorized")
             const updated = await updateClient(body);
