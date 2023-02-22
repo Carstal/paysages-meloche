40cafcc5f4759de9656ced599f9a7251fbfa34bb
YanBurton <yanburton03@gmail.com>
2023-02-21 21:39:34 -0500

    Quick fix

--- nextjs-blog/pages/api/forms/newClientForm.js
+++ nextjs-blog/pages/api/forms/newClientForm.js
@@ -1,11 +1,13 @@
-import { updateClient } from "../../../src/components/client/client_service"
+import { updateClient } from "../../../src/components/client/client_service";
 // const Client = require("../../../src/components/client/client");
 
 export default async function handler(req, res) {
-    var body = req.body
+  var body = req.body;
 
-    //map to object if necessary later
-    const updated = await updateClient(body);
+  //map to object if necessary later
+  const updated = await updateClient(body);
 
-    res.redirect('http://localhost:3000/api/auth/logout?returnTo=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Flogin')
+  res.redirect(
+    "https://paysages-meloche.vercel.app/api/auth/logout?returnTo=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Flogin"
+  );
 }
